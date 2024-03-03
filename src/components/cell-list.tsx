b38603f6925f09089cd192d970d6fc0ef7a87e05
A3thyr <a3therys@gmail.com>
2024-03-03 14:41:52 +0300

    feat: added useTypedSelector hook, few main components

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 const CellList: FC = () => {
+  useTypedSelector((state) => state);
   return <div>cell-list</div>;
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 export const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
